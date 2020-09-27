4a72ec86d75fffc9091841c287a602583651dd51
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-09-26 22:11:28 -0500

    Init firebase and adding logic to get data from clients doc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB8_hIoAPnuqE1qzsKuUB5jazm30_fgq3o',
+    authDomain: 'client-panel-bfb34.firebaseapp.com',
+    databaseURL: 'https://client-panel-bfb34.firebaseio.com',
+    projectId: 'client-panel-bfb34',
+    storageBucket: 'client-panel-bfb34.appspot.com',
+    messagingSenderId: '880213260480',
+    appId: '1:880213260480:web:9a025672ba78d218795525',
+    measurementId: 'G-R2156EKMP0'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB8_hIoAPnuqE1qzsKuUB5jazm30_fgq3o',
+    authDomain: 'client-panel-bfb34.firebaseapp.com',
+    databaseURL: 'https://client-panel-bfb34.firebaseio.com',
+    projectId: 'client-panel-bfb34',
+    storageBucket: 'client-panel-bfb34.appspot.com',
+    messagingSenderId: '880213260480',
+    appId: '1:880213260480:web:9a025672ba78d218795525',
+    measurementId: 'G-R2156EKMP0'
+  }
 };
 
 /*
